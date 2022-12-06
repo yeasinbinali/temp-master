2a516fc428cb2df9a2b9427302be06cd6c8c9f88
yeasinbinali <yeasinbinali@gmail.com>
2022-12-06 12:55:26 +0600

    temp-degree & condition added

--- js/app.js
+++ js/app.js
@@ -5,17 +5,19 @@ const searchTemp = () => {
     tempField.value = '';
 
     const API_KEY = '5fdd1fff46a413d35392afa14e4ea8cd'
-    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${tempFieldValue}&appid=${API_KEY}`
+    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${tempFieldValue}&appid=${API_KEY}&units=metric`
     fetch(weatherAPI)
         .then(res => res.json())
         .then(data => displayTemp(data))
 }
 
-const tempPlace = (id, city) => {
+const temperatureInnerText = (id, city) => {
     document.getElementById(id).innerText = city;
 }
 
 const displayTemp = temperature => {
     console.log(temperature)
-    tempPlace('temp-place', temperature.name);
+    temperatureInnerText('temp-place', temperature.name);
+    temperatureInnerText('temp-degree', temperature.main.temp);
+    temperatureInnerText('condition', temperature.weather[0].main);
 }
